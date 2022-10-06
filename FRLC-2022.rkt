ccbc403e9ca29f52c3e8ece47faec03d77b8865a
thomasGarau <thomas.garau8@gmail.com>
2022-10-06 14:24:20 +0200

    start

---
+++ FRLC-2022.rkt
@@ -0,0 +1,151 @@
+(require (lib "trace.ss"))
+
+(require (lib "compat.ss"))
+
+(require (lib "list.ss"))
+(define *frames* '())
+(define *frame '())
+(define *slot '())
+(define *facet '())
+(define *value '())
+
+(define (fget frame slot facet)
+  (map car 
+       (mycdr (myassoc  facet 
+                        (mycdr (myassoc slot 
+                                        (mycdr (mygetprop frame 'frame))))))))
+
+(define (mycdr l)
+  (cond ((null? l) '())
+        (#t (cdr l))))
+
+(define (mygetprop s p) 
+  (cond ((equal? #f (getprop s p)) '())
+        (#t (getprop s p))))
+
+(define (fassoc-slot frame cle aliste)
+  (cond ((assoc  cle (cdr aliste)))
+        (#t    (putprop frame 'frame (ajoute-slot cle aliste (getprop frame 'frame))) (myassoc cle (cdr (getprop frame 'frame)))
+               )))
+
+
+(define (fassoc-facet frame slot facet cle aliste)
+  
+  
+  
+  (cond ((null? aliste) '())
+        ((assoc  cle (cdr aliste)))
+        ;((
+        (#t    (putprop frame 'frame (ajoute-facet  facet slot cle aliste (getprop frame 'frame))) (myassoc cle (cdr (myassoc slot (cdr (getprop frame 'frame)))))
+               )))
+
+(define (fassoc-value  frame slot facet  value cle aliste)
+  
+  (cond ((null? aliste) '())
+        ((assoc  cle (cdr aliste)))
+        (#t (putprop frame 'frame (ajoute-value  value facet  slot 
+                                                 cle aliste (getprop frame 'frame)))
+            ))) 
+
+(define (rplacd l l1)
+  (cons (car l) l1))
+
+(define (rplacd2 lambda l l1)
+  (set-rest! l l1) (cons (car l) l1))
+
+
+(define (ajoute-slot cle aliste  l)
+  (cond ((null? (cdr l)) (list (car l) (cons cle (cdr l))))
+        (#t (cons (car l) (cons (cons cle (cdr aliste)) (cdr l))))))
+
+(define (removemy s l)
+  (cond ((null? l) '())
+        ((equal? s (car l))(removemy s (cdr l)))
+        (else (cons (car l) (removemy s (cdr l)))))) 
+
+
+(define (ajoute-facet  facet slot cle aliste  l) 
+  
+  (cons (car l) (cons (cons  slot  (cons (list facet) (cdr aliste))) (remove (myassoc slot (cdr l)) (cdr l)))))
+
+(define (ajoute-value value facet  slot cle aliste  l)
+  ;(cons (car l)(cons (cons slot (cons (cons facet (cons (list value) (cdr aliste))) (myassoc facet (cdr aliste)))) (remove (myassoc slot (cdr l)) (cdr l))))) 
+  (cons (car l)(cons (append (list slot) (cons (cons facet (cons (list value) (cdr aliste))) (myassoc facet (cdr aliste))) (liste-facets (car l)  facet slot l)  ) (remove (myassoc slot (cdr l)) (cdr l)))))  
+
+(define (liste-facets frame facet slot l)
+  
+  (cond ((not (null? (mycdr (mycdr (myassoc slot (cdr l)))))) (cond ((not (member (caar (mycdr (mycdr (myassoc slot (cdr l)))))  (list-slots l)))   (mycdr (cdr (assoc slot (cdr l)))))
+                                                                    (#t '()))) 
+        
+        (#t '())))
+
+
+(define (list-slots l)
+  (map (lambda ( ls)(car ls)  )  (cdr  (fgetframe (car l))))) 
+
+(define (myassoc cle aliste)
+  (cond ((equal? #f (assoc cle aliste)) '())
+        ((assoc cle  aliste))))
+
+(define (fput frame slot facet valeur)
+  (cond ((equal? valeur (mycar (fget frame slot facet))) #f)
+        (#t  (fassoc-value frame slot facet valeur valeur
+                           (fassoc-facet frame slot  facet facet
+                                         (fassoc-slot frame slot
+                                                      (fgetframe frame)))) valeur)  ))  
+
+(define (mycar l)(cond ((null? l) '())
+                       (#t (car l))))
+
+(define fgetframe (lambda (frame)
+                    (cond ((getprop frame 'frame))
+                          (#t (set! *frames* (append *frames* (list frame))) 
+                              (putprop frame  'frame (list frame))))
+                    (getprop frame 'frame) )) 
+
+
+(define last (lambda (l)
+               (cond ( (not (null? l)) (list (car (reverse l))))
+                     (#t '()))))
+
+
+
+
+
+(define (fcreate frame name)
+  (fput name 'ako 'valeur frame)
+  (fput name 'classification 'valeur 'instance))
+
+(define (finst frame name)
+  (fcreate frame name) 
+  (set! *frame name)
+  (map(lambda(e)
+        (map (lambda (slot)
+        (cond((not (null?(fget e slot 'valeur))))
+             (#t (cond((not (null? (fget e slot 'defaut))))
+                      (#t (set! *slot slot)(apply(eval (mycar (fget e slot 'ifneeded))) '()))))))
+               (fslot e)))
+      (fgetclasses frame)))
+
+(define (fgetclasses frame)
+  (cond((null? frame) '())
+       ((equal? frame 'objet) '(objet))
+      (#t (cons frame (fgetclasses(mycar(fget frame 'ako 'valeur)))))))  
+
+(define(fslot frame)
+  (map car(mycdr (mygetprop frame 'frame))))
+
+(define (ffacet frame slot)
+(map car (mycdr (myassoc slot (mycdr (mygetprop frame 'frame))))))
+
+(fput 'homme 'vie 'defaut 'vivant)
+(fput 'homme 'ako 'valeur 'objet)
+(fput 'homme 'travail 'ifneeded 'ask)
+(fput 'homme 'marié 'defaut 'non)
+(fput 'homme 'mere 'defaut 'inconnue)
+(fput 'femme 'ako 'valeur 'objet)
+(fput 'femme 'vie 'defaut 'vivant)
+(fput 'femme 'travail 'defaut 'aucun)
+(fput 'femme 'marié 'defaut 'non)
+(fput 'femme 'enfant 'defaut 0)
+(fput 'femme 'mere 'defaut 'inconnue)
