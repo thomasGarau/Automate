751a13fa6ebdc18e59af840a265963746bd60c0f
Donat Fortini <donatfortini2a@gmail.com>
2022-10-20 17:09:12 +0200

    fwriteframe

--- FRLC-2022.rkt
+++ FRLC-2022.rkt
@@ -340,6 +340,14 @@
   (send fenetre show #t))
 
 
+(define (Fwriteframe frame)
+  (define ret "|" )
+  (set! ret (string-append ret (symbol->string (car(fgetframe frame))) ":" " \n "))
+  (define listslot (fslot frame))
+  (map(lambda(e)
+    (set! ret (string-append ret (symbol->string e) ":" " \n " ))
+    (set! ret (string-append ret (symbol->string (car(ffacet frame e))) "->" (symbol->string (car (fget frame e (car(ffacet frame e))))) " \n "))
+    )listslot) ret)
 
 (fput 'homme 'vie 'defaut 'vivant)
 (fput 'homme 'ako 'valeur 'objet)
