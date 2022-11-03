7fc97819451538192d9f753e6c0e5d2b40fae5fe
Donat Fortini <donatfortini2a@gmail.com>
2022-11-03 16:29:15 +0100

    doublon

--- FRLC-2022.rkt
+++ FRLC-2022.rkt
@@ -441,35 +441,7 @@
                                                                                                                 (#t (string->symbol (list-ref l (+ e 3)))))
   ))len))  
   
-<<<<<<< HEAD
-  (define (Refresh frame panel)
-    (define text(new text%))
-    (send panel set-editor text)
-    (send text auto-wrap #t)
-    (send text set-padding 10 10 10 10)
-    (define f (symbol->string (car(fgetframe frame))))
-    (define listslot (fslot frame))
-    (send text insert (make-object string-snip% f))
-    (send text insert (make-object string-snip% f)))
-    ;(send fenetre show #t))
 
-
-(define (Fwriteframe frame)
-  (define ret "|" )
-  (set! ret (string-append ret (symbol->string (car(fgetframe frame))) ":" " \n "))
-  (define listslot (fslot frame))
-  (map(lambda(e)
-    (set! ret (string-append ret (symbol->string e) ":" " \n " ))
-    (set! ret (string-append ret (symbol->string (car(ffacet frame e))) "->" (symbol->string (car (fget frame e (car(ffacet frame e))))) " \n "))
-    )listslot) ret)
-
-(define (Fimprim frame)
-  (define out (open-output-file "sauvegarde.txt" #:exists 'truncate))
-  (println (Fwriteframe frame) out)
-  (close-output-port out))
-=======
-  
->>>>>>> e7f4b13c5acb1881c623ef223446f43ee200b673
 
 (fput 'homme 'vie 'defaut 'vivant)
 (fput 'homme 'classification 'valeur 'prototype)
